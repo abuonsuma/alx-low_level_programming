d0af9822803fdfc2f2b1325e694b7ff411ed30b4
abuonsuma <abuonsuma@gmail.com>
2023-08-10 20:09:57 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
-/**
- * main - Entry point
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- * followed by a new line.
- * Return: Always 0 (success)
- */ */
+ * main - writes to the stderr using
+ * Description: the program writes to stderr.
+ * Return: value 1 on success.
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
