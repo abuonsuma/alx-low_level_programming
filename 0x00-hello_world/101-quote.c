299cafdee9aa851736b6efd5d3451744ac7c0bc3
abuonsuma <abuonsuma@gmail.com>
2023-08-10 19:48:05 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, 59);
+	return 1;
 }
+
