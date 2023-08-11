3b247ac81e45e285ae4fe8e8245af366bd44491f
abuonsuma <abuonsuma@gmail.com>
2023-08-10 19:28:50 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  * description: the program writes to stderr.
  * return: value 1 on success.
  */
-int main(void)
-{
+int main(void) {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	
 	return (1);
