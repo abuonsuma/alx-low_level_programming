6aa76e8d4b3e8f63bbd1132cb6882f966a9c8863
abuonsuma <abuonsuma@gmail.com>
2023-08-10 20:00:45 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  *main - prints a string
  *return: 0(Success)
+ *description: program prints without error
  */
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
