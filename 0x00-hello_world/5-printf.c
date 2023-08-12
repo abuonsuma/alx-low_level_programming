7745dc98186974bfe98bab2f458370e4cda94714
abuonsuma <abuonsuma@gmail.com>
2023-08-11 17:38:49 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- * main - writes to the stderr using
+ * main - main function
  *
- * Return: value 1 on success.
+ * Return: 0 (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
 }
