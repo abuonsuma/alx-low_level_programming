6a55311bcc0f2789fb7342e5a55472a23e68c7af
abuonsuma <abuonsuma@gmail.com>
2023-08-12 19:56:55 -0700

    lower case rev.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: print lowercase alphabet backwards
+ *Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; --c)
+putchar(c);
+
+putchar('\n');
+
+return (0);
+}
