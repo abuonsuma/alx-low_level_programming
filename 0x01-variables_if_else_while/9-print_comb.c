8969d6628cf3af9591868c69ebab5f4e134640c5
abuonsuma <abuonsuma@gmail.com>
2023-08-12 20:10:06 -0700

    possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,17 +2,19 @@
 
 /**
  *main - Start of program execution
- /*
-  *loop for numbers 0-9
-  *if number is 0-9, print number
-  *then if number is 0-8, add comma plus space
-  *else if number is 9, add only space
-  *Return: 0(Success)
- */
+ *
+ *Return: 0(Success)
+*/
 int main(void)
 {
 	int num;
 
+	/*
+	 *loop for numbers 0-9
+	 *if number is 0-9, print number
+	 *then if number is 0-8, add comma plus space
+	 *else if number is 9, add only space
+	 */
 	for (num = '0'; num <= '9'; num++)
 		if (num >= '0')
 		{
