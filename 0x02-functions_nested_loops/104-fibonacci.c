7b9d797e7e0d47bf91e0bc697ecfed6516ed7dbd
abuonsuma <abuonsuma@gmail.com>
2023-08-16 19:18:52 -0700

    adv task

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - print first 98 fibonacci numbers
+ *
+ *Return: 0(Success)
+ */
+
+int main(void)
+{
+	long int i, q = 1, r = 2, sum = 0;
+
+	for (i = 0; i < 97; i++)
+	{
+		printf("%lu, ", q);
+		sum = q + r;
+		q = r;
+		r = sum;
+		if (i == 96)
+			printf("%lu\n", x);
+	}
+
+	return (0);
+}
