33f9808265ed07aa7a05911d041c9d3811350a9f
abuonsuma <abuonsuma@gmail.com>
2023-08-16 18:54:56 -0700

    adv task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Start of function
+ *
+ *Return: 0(Success)
+ */
+int main(void)
+{
+	int i;
+	int s = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			s += i;
+		}
+	}
+
+	printf("%d\n", s);
+
+	return (0);
+}
