b6190782c4eef168a9da26a6a2a68d45124b67a3
abuonsuma <abuonsuma@gmail.com>
2023-08-16 20:02:48 -0700

    adv task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,58 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- *print_times_table - prints tables 1 to n
- *
- *Return: void
-*/
-void print_times_table(int n)
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	int q, r, product;
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-	for (q = 0; q <= n; q++)
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
 	{
-		
-		for (r = 0; r <= n; r++)
+		if ((i % 3) == 0)
 		{
-			product = q * r;
-			
-			if (product < 10 && r != n && r != 0)
-			{
-				_putchar(' ');
-				_putchar(product + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			
-			else if (product >= 10 && r != n)
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			
-			else if (product < 10 && r == n && r != 0)
-			{
-				_putchar(' ');
-				_putchar(product + '0');
-			}
-			
-			else if (r == 0)
-			{
-				_putchar('0' + product);
-				_putchar(',');
-				_putchar(' ');
-			}
-
-			else
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
 		}
-
-		_putchar('\n');
 	}
-
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
 }
