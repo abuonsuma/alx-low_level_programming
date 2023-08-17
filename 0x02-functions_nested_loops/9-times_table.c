f15005c8faf1abc52bd3a1896f1ccbbf91d9ab3d
abuonsuma <abuonsuma@gmail.com>
2023-08-16 18:27:56 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			else if (prod >= 10 && r != 9)
+			else if (product >= 10 && r != 9)
 			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
