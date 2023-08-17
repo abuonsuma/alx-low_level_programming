af72817c4252214547807dbec5d1128be771a146
abuonsuma <abuonsuma@gmail.com>
2023-08-16 18:49:53 -0700

    adv task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ *print_times_table - prints tables 1 to n
+ *
+ *Return: void
+*/
+void print_times_table(int n)
+{
+	int q, r, product;
+
+	for (q = 0; q <= n; r++)
+	{
+		
+		for (r = 0; r <= n; r++)
+		{
+			prod = q * r;
+			
+			if (product < 10 && r != n && r != 0)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			
+			else if (product >= 10 && r != n)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((produst % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			
+			else if (product < 10 && r == n && b != 0)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			
+			else if (r == 0)
+			{
+				_putchar('0' + product);
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+
+}
