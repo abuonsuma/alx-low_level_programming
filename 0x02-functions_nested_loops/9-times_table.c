967b48ff9c67d598b2da514484554a87614ceec9
abuonsuma <abuonsuma@gmail.com>
2023-08-16 18:09:22 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ *times_table - prints tables 1 to 9
+ *
+ *Return: void
+*/
+void times_table(void)
+{
+	int q, r, prod;
+
+	for (q = 0; q <= 9; q++)
+	{
+		for (r = 0; r <= 9; r++)
+		{
+			prod = q * r;
+			if (product < 10 && r != 9 && r != 0)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (prod >= 10 && r != 9)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (product < 10 && r == 9 && r != 0)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (b == 0)
+			{
+				_putchar('0' + product);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
