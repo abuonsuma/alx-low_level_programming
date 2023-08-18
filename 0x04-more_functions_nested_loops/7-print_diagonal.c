8588ab8382e497bc347e6ed3c536be594c72bcc9
abuonsuma <abuonsuma@gmail.com>
2023-08-17 18:33:11 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ * @n: is the number of times the \ character
+ * should be printed
+*/
+
+void print_diagonal(int n)
+{
+	int point, gap;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (point = 1; point <= n; ++point)
+		{
+			for (gap = 1; gap <= point; ++gap)
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
+			_putchar('\n');
+		}
+	}
+}
