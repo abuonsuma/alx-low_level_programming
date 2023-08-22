1a7d1165dc32e5dd1b00b83809240a824ccb5055
abuonsuma <abuonsuma@gmail.com>
2023-08-21 18:32:08 -0700

    Advanced task 1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int q, r, n, len, f, digit;
+
+	q = 0;
+	r = 0;
+	x = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (q < len && f == 0)
+	{
+		if (s[q] == '-')
+			++r;
+
+		if (s[q] >= '0' && s[q] <= '9')
+		{
+			digit = s[q] - '0';
+			if (r % 2)
+				digit = -digit;
+			x = x * 10 + digit;
+			f = 1;
+			if (s[q + 1] < '0' || s[q + 1] > '9')
+				break;
+			f = 0;
+		}
+		q++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
